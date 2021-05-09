9ab84f79424bf9ea3b6dbaffdc1a1e0b7fc7ffc2
Sajed Almorsy <sajed.almorsy@gmail.com>
2021-05-09 09:40:19 +0200

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root :to => 'simple_calculator#index'
+  namespace :operation do
+    post :binary_operations, to: 'binary_operations#create_or_update', as: :binary_operations_create_or_update
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
